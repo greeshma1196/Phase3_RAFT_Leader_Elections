fc1f4396b761792b901983fa40e1c2976c050f3c
Greeshma Ann Mathew <greeshma.mathew@gmail.com>
2022-04-08 12:31:15 -0400

    Finalized code

--- Controller/convert_follower_node1.py
+++ Controller/convert_follower_node1.py
@@ -11,14 +11,17 @@ msg = json.load(open("Message.json"))
 
 # Initialize
 sender = socket.gethostbyname('localhost')
-print(f"Sender: {sender}")
 target = "Node1"
 port = 5555
 
 # Request
 msg['sender_name'] = sender
-msg['request'] = "CONVERT_FOLLOWER"
-print(f"Request Created : {msg}")
+# Developer's note: Update the request to either of the following:
+    # CONVERT_FOLLOWER
+    # TIMEOUT
+    # SHUTDOWN
+    # LEADER_INFO
+msg['request'] = "LEADER_INFO" 
 
 # Socket Creation and Binding
 skt = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -27,11 +30,12 @@ skt.bind((sender, port))
 # Send Message
 try:
     # Encoding and sending the message
-    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5000))
-    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5001))
-    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5002))
-    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5003))
-    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5004))
+    # Developer's note: Comment/Uncomment any of the following nodes with the request message
+    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5000)) #node1
+    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5001)) #node2
+    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5002)) #node3
+    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5003)) #node4
+    skt.sendto(json.dumps(msg).encode('utf-8'), (sender, 5004)) #node5
 except:
     #  socket.gaierror: [Errno -3] would be thrown if target IP container does not exist or exits, write your listener
     print(f"ERROR WHILE SENDING REQUEST ACROSS : {traceback.format_exc()}")
--- Node/main.py
+++ Node/main.py
@@ -1,7 +1,5 @@
-from concurrent.futures import thread
-from email import message
 import json
-from logging import shutdown
+import sys
 import threading
 import os
 import time
@@ -17,9 +15,9 @@ port = int(os.getenv('PORT'))
 
 election_timeout_interval = (3*heartbeat_interval) + (randrange(500,1000)/1000)
 
-RAFT_LEADER = "leader"
-RAFT_FOLLOWER = "follower"
-RAFT_CANDIDATE = "candidate"
+RAFT_LEADER = "LEADER"
+RAFT_FOLLOWER = "FOLLOWER"
+RAFT_CANDIDATE = "CANDIDATE"
 
 log = []
 prev_log_index = -1
@@ -41,183 +39,24 @@ RESPONSE_VOTE_DENIED = "VOTE_DENIED"
 RESPONSE_VOTE_GRANTED = "VOTE_ACK"
 RESPONSE_ENTRY_APPENDED = "ENTRY_APPENDED"
 RESPONSE_LEADER_INFO = "RESPONSE_LEADER_INFO"
+RESPONSE_SHUTDOWN_NODE = "RESPONSE_SHUTDOWN_NODE"
 
-
-def handle_request_get_vote(candidate_term, candidate_id, socket):
-
-    global current_term, voted_for
-    
-    if current_term >= candidate_term:
-        print(f"{my_node_id} denied vote for {candidate_id} because current term is {current_term} and candidate term is {candidate_term}")
-        message = {
-            "sender_name": my_node_id,
-            "request": RESPONSE_VOTE_DENIED,
-            "term": current_term,
-            "previous_log_index": prev_log_index,
-            "previous_log_term": prev_log_term,
-            "entries": log
-        } 
-        
-        message = json.dumps(message).encode()
-        return socket.sendto(message, (candidate_id, port))
-    
-    if voted_for and voted_for != candidate_id:
-        print(f"{my_node_id} denied vote for {candidate_id} because follower node has voted for {voted_for} and candidate id is {candidate_id}")
-        message = {
-            "sender_name": my_node_id,
-            "request": RESPONSE_VOTE_DENIED,
-            "term": current_term,
-            "previous_log_index": prev_log_index,
-            "previous_log_term": prev_log_term,
-            "entries": log
-        } 
-        
-        message = json.dumps(message).encode()
-        return socket.sendto(message, (candidate_id, port))
-    
-    print(f"Vote granted by {my_node_id} to {candidate_id} and the term is {candidate_term}")
-    
-    current_term = candidate_term
-    voted_for = candidate_id
-
-    message = {
-        "sender_name": my_node_id,
-        "request": RESPONSE_VOTE_GRANTED,
-        "term": current_term,
-        "previous_log_index": prev_log_index,
-        "previous_log_term": prev_log_term,
-        "entries": log
-    }
-
-    message = json.dumps(message).encode()
-    return socket.sendto(message, (candidate_id, port))
-
-def handle_response_vote_granted(socket):
-    global votes_received, current_raft_state
-
-    votes_received += 1
-
-    if votes_received < len(all_nodes)/2:
-        return
-
-    current_raft_state = RAFT_LEADER
-
-    print(f"{my_node_id} is the new {current_raft_state} and the term is {current_term}, votes received {votes_received} and {len(all_nodes)/2}")
-
-
-    message = {
-        "sender_name": my_node_id,
-        "request": REQUEST_APPEND_ENTRY,
-        "term": current_term,
-        "previous_log_index": prev_log_index,
-        "previous_log_term": prev_log_term,
-        "entries": log
-    }
-
-    message = json.dumps(message).encode()
-    return socket.sendto(message, (my_node_id, port))
-
-def handle_request_append_entry(leader_term, leader_id, socket):
-
-    global voted_for, last_heartbeat_received
-    
-    if current_term > leader_term:
-        return
-    
-    last_heartbeat_received = time.time()
-
-    #current election has ended
-    #reseting voted_for so that it can cast a new vote in the next election term
-    voted_for = ""
-
-    message = {
-        "sender_name": my_node_id,
-        "request": RESPONSE_ENTRY_APPENDED,
-        "term": current_term,
-        "previous_log_index": prev_log_index,
-        "previous_log_term": prev_log_term,
-        "entries": log
-    } 
-        
-    message = json.dumps(message).encode()
-    return socket.sendto(message, (leader_id, port))
-
-def handle_request_convert_follower():
-    global current_raft_state, voted_for
-
-    current_raft_state = RAFT_FOLLOWER
-    voted_for = ""
-
-    print(f"{my_node_id} is in {current_raft_state}")
-
-def handle_request_leader_info():
-
-    if current_raft_state != RAFT_LEADER:
-        return 
-    
-    return {"Raft_State":current_raft_state, "Node":my_node_id}
-  
-def handle_request_timeout():
-
-    global current_raft_state, voted_for
-
-    current_raft_state = RAFT_FOLLOWER
-    voted_for = ""
-
-
-class RequestHandler(socketserver.DatagramRequestHandler):
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        if len(data) == 0:
-            return
-
-        data = json.loads(data)
-
-        if data["request"] == REQUEST_CONVERT_FOLLOWER:
-            print(f"Data: {data}")
-
-        sender_name = data.get("sender_name", None)
-        term = data.get("term", None)
-        request = data.get("request", None)
-        previous_log_index = data.get("previous_log_index", None)
-        previous_log_term = data.get("previous_log_term", None)
-        entries = data.get("entries", None)
-
-        if request == REQUEST_GET_VOTE:
-            return handle_request_get_vote(term, sender_name, socket)
-
-        if request == RESPONSE_VOTE_GRANTED:
-            return handle_response_vote_granted(socket)
-        
-        if request == REQUEST_APPEND_ENTRY:
-            return handle_request_append_entry(term, sender_name, socket)
-        
-        if request == REQUEST_CONVERT_FOLLOWER:
-            #set a delay to prevent abrupt behaviour
-            time.sleep(election_timeout_interval)
-            return handle_request_convert_follower()
-        
-        if request == REQUEST_LEADER_INFO:
-            leader_info = {}
-            leader_info = handle_request_leader_info()
-            print (f"{leader_info['Node']} is the new {leader_info['Raft_State']}")
-
-        if request == REQUEST_SHUTDOWN:
-            print(f"Shutting down service {my_node_id}")
-            self.server.shutdown()
-
-        if request == REQUEST_TIMEOUT:
-            return handle_request_timeout()
-            
+__wait_check = False
+__request_shutdown_check = False
 
 def send_heartbeats(socket):
 
     while True:
         
+        if __request_shutdown_check:
+            time.sleep(heartbeat_interval)
+            break
+
+        #check if the raft state is not leader
         if current_raft_state != RAFT_LEADER:
             continue
-
+        
+        #if the check fails then it will send a heartbeat to all nodes except itself
         for node in all_nodes:
 
             if node == my_node_id:
@@ -232,40 +71,56 @@ def send_heartbeats(socket):
                 "entries": log
             }
 
-            
-
             message = json.dumps(message).encode()
-            socket.sendto(message, (node, port))
+            try:
+                socket.sendto(message, (node, port))
+            except OSError:
+                pass
         
         time.sleep(heartbeat_interval)
 
-def election_timeout(socket):
+def election_timeout(socket):   
 
-    global current_term, current_raft_state, votes_received, voted_for
+    global current_term, current_raft_state, votes_received, voted_for, election_timeout_interval, __wait_check
 
-    print(f"Node ID {my_node_id} has an election timeout of {election_timeout_interval}")
-    
+    print(f"Node ID {my_node_id} has an election timeout of {election_timeout_interval}")   
 
     while True:
-        
+
         time.sleep(election_timeout_interval)
 
+        if __request_shutdown_check:
+            time.sleep(election_timeout_interval)
+            break
 
+        if __wait_check: 
+            __wait_check = False
+            time.sleep(election_timeout_interval)
+            continue
+
+        #check if raft state is leader
         if current_raft_state == RAFT_LEADER:
+            time.sleep(election_timeout_interval)
             continue
         
+        #if raft state is not leader, check if the node has voted for any other node
         if voted_for:
             continue
         
-        print(f"Last Heartbeat Received: {last_heartbeat_received}")
+        #if the previous check fails, then check to see if you have received a heartbeat within the election timeout interval
         if time.time() - last_heartbeat_received < election_timeout_interval:
+            time.sleep(election_timeout_interval)
             continue
         
+        #if all the check fail, set the state to candidate state
+        #update the current term of the node
+        #make the node vote for itself and increment the votes_received
         current_raft_state = RAFT_CANDIDATE
         current_term += 1
         voted_for = my_node_id
         votes_received = 1
 
+        #send a request to get vote from all the other nodes in the cluster
         for node in all_nodes:
 
             #to not send message to itself
@@ -282,15 +137,262 @@ def election_timeout(socket):
             }
 
             message = json.dumps(message).encode()
-            socket.sendto(message, (node, port))
+            try:
+                socket.sendto(message, (node, port))
+            except OSError:
+                pass
 
+def handle_request_get_vote(candidate_term, candidate_id, socket):
 
+    global current_term, voted_for
+    
+    #check if node's term if greater than or equal to the candidate node's term
+    if current_term >= candidate_term:
+        print(f"{my_node_id} denied vote for {candidate_id} because current term is {current_term} and candidate term is {candidate_term}")
+        message = {
+            "sender_name": my_node_id,
+            "request": RESPONSE_VOTE_DENIED,
+            "term": current_term,
+            "previous_log_index": prev_log_index,
+            "previous_log_term": prev_log_term,
+            "entries": log
+        } 
+        
+        message = json.dumps(message).encode()
+        return socket.sendto(message, (candidate_id, port))
+    
+    #check if the node has voted for any node other than the candidate node
+    if voted_for and voted_for != candidate_id:
+        print(f"{my_node_id} denied vote for {candidate_id} because follower node has voted for {voted_for} and candidate id is {candidate_id}")
+        message = {
+            "sender_name": my_node_id,
+            "request": RESPONSE_VOTE_DENIED,
+            "term": current_term,
+            "previous_log_index": prev_log_index,
+            "previous_log_term": prev_log_term,
+            "entries": log
+        } 
+        
+        message = json.dumps(message).encode()
+        return socket.sendto(message, (candidate_id, port))
+    
+    print(f"Vote granted by {my_node_id} to {candidate_id} and the term is {candidate_term}")
+    
+    #if the above checks fail then the node will grant a vote
+    #it will update it's term to the candidate node's term
+    current_term = candidate_term
+    voted_for = candidate_id
+
+    message = {
+        "sender_name": my_node_id,
+        "request": RESPONSE_VOTE_GRANTED,
+        "term": current_term,
+        "previous_log_index": prev_log_index,
+        "previous_log_term": prev_log_term,
+        "entries": log
+    }
+
+    message = json.dumps(message).encode()
+    return socket.sendto(message, (candidate_id, port))
+
+def handle_response_vote_granted(socket):
+    global votes_received, current_raft_state, leader_id, election_timeout_interval
+
+    votes_received += 1
+
+    #check if the votes received is less than half of the total number of nodes
+    if votes_received < len(all_nodes)/2:
+        return
+
+    #if the votes received are greater than half the total number of nodes in the cluster, then set the raft state of the candidate node to leader
+    #send append entry request as the new leader
+    current_raft_state = RAFT_LEADER
+    leader_id = my_node_id
+
+    print(f"{my_node_id} is the new {current_raft_state} and the term is {current_term}, votes received {votes_received} and {len(all_nodes)/2}")
+
+    message = {
+        "sender_name": my_node_id,
+        "request": REQUEST_APPEND_ENTRY,
+        "term": current_term,
+        "previous_log_index": prev_log_index,
+        "previous_log_term": prev_log_term,
+        "entries": log
+    }
+
+    message = json.dumps(message).encode()
+    return socket.sendto(message, (my_node_id, port))
+
+def handle_request_append_entry(leader_term, leader_node_id, socket):
+
+    global voted_for, last_heartbeat_received, leader_id, current_raft_state
+    
+    #if the node's term is greater than the leader's term then it will not accept any entry request from the leader
+    if current_term > leader_term:
+        return
+    
+    leader_id = leader_node_id
+    #current election has ended
+    last_heartbeat_received = time.time()
+
+    # if current_raft_state != RAFT_FOLLOWER:
+    #     current_raft_state = RAFT_FOLLOWER
+
+    #reseting voted_for so that it can cast a new vote in the next election term
+    voted_for = ""
+
+    message = {
+        "sender_name": my_node_id,
+        "request": RESPONSE_ENTRY_APPENDED,
+        "term": current_term,
+        "previous_log_index": prev_log_index,
+        "previous_log_term": prev_log_term,
+        "entries": log
+    } 
+        
+    message = json.dumps(message).encode()
+    try:
+        socket.sendto(message, (leader_node_id, port))
+    except:
+        pass
+
+def handle_request_convert_follower():
+    global current_raft_state, voted_for, last_heartbeat_received, __wait_check
+
+    time.sleep(randrange(500,1000)/1000)
+    #set the raft state of the node to follower and reset voted_for so that it can cast a new vote in the next election term
+    current_raft_state = RAFT_FOLLOWER
+    voted_for = ""
+    __wait_check = True
+
+    print(f"{my_node_id} is in {current_raft_state}")
+
+def handle_request_leader_info(controller_address, socket):
+    
+    print(f"This is {my_node_id} and my leader is {leader_id} for term {current_term}")
+    message = {
+        "sender_name": my_node_id,
+        "request": RESPONSE_LEADER_INFO,
+        "term": current_term,
+        "previous_log_index": prev_log_index,
+        "previous_log_term": prev_log_term,
+        "entries": log,
+        "key": "LEADER",
+        "value": leader_id
+    }
+
+    message = json.dumps(message).encode()
+    socket.sendto(message, controller_address)
+  
+def handle_request_timeout(socket):
+
+    global current_raft_state, voted_for, votes_received, current_term
+
+    current_raft_state = RAFT_CANDIDATE
+    current_term += 1
+    voted_for = my_node_id
+    votes_received = 1
+
+        #send a request to get vote from all the other nodes in the cluster
+        #print(f"All nodes in election timeout:{all_nodes}")
+    for node in all_nodes:
+
+        #to not send message to itself
+        if node == my_node_id:
+            continue
+        
+        message = {
+            "sender_name": my_node_id,
+            "request": REQUEST_GET_VOTE,
+            "term": current_term,
+            "previous_log_index": prev_log_index,
+            "previous_log_term": prev_log_term,
+            "entries": log
+        }
+
+        message = json.dumps(message).encode()
+        socket.sendto(message, (node, port))
+
+def handle_request_shutdown(socket):
+
+    global __request_shutdown_check, all_nodes
+
+    all_nodes.remove(my_node_id)
+    __request_shutdown_check = True
+    print(f"Shutting down service {my_node_id} and the remianing nodes are {all_nodes}")
+    for node in all_nodes:
+        if node == my_node_id:
+            continue
+        message = {
+            "sender_name": my_node_id,
+            "request": RESPONSE_SHUTDOWN_NODE,
+            "term": current_term,
+            "previous_log_index": prev_log_index,
+            "previous_log_term": prev_log_term,
+            "entries": log
+        }
+        message = json.dumps(message).encode()
+        socket.sendto(message, (node, port))
+
+def handle_response_shutdown(node):
+
+    global all_nodes
+
+    all_nodes.remove(node)
+
+class RequestHandler(socketserver.DatagramRequestHandler):
+    def handle(self):
+
+        #pre-process the data
+        data = self.request[0].strip()
+        socket = self.request[1]
+        controller_address = self.client_address
+       
+        if len(data) == 0:
+            return
+
+        data = json.loads(data)
+
+        sender_name = data.get("sender_name", None)
+        term = data.get("term", None)
+        request = data.get("request", None)
+        previous_log_index = data.get("previous_log_index", None)
+        previous_log_term = data.get("previous_log_term", None)
+        entries = data.get("entries", None)
+
+        #check for thre request being passed
+        if request == REQUEST_GET_VOTE:
+            return handle_request_get_vote(term, sender_name, socket)
+
+        if request == RESPONSE_VOTE_GRANTED:
+            return handle_response_vote_granted(socket)
+        
+        if request == REQUEST_APPEND_ENTRY:
+            return handle_request_append_entry(term, sender_name, socket)
+        
+        if request == REQUEST_CONVERT_FOLLOWER:
+            #set delay to prevent abrupt behaviour
+            handle_request_convert_follower()
+        
+        if request == REQUEST_LEADER_INFO:
+            handle_request_leader_info(controller_address, socket)
+
+        if request == REQUEST_SHUTDOWN:
+            handle_request_shutdown(socket)
+        
+        if request == RESPONSE_SHUTDOWN_NODE:
+            handle_response_shutdown(sender_name)
+
+        if request == REQUEST_TIMEOUT:
+            return handle_request_timeout(socket)
+            
 if __name__ == "__main__":
 
     with socketserver.ThreadingUDPServer(('0.0.0.0', port), RequestHandler) as server:
-        threading.Thread(target=election_timeout, args=[server.socket]).start()
-        threading.Thread(target=send_heartbeats, args=[server.socket]).start()
+        election_timeout_thread = threading.Thread(target=election_timeout, args=[server.socket])
+        send_heartbeats_thread = threading.Thread(target=send_heartbeats, args=[server.socket])
+        election_timeout_thread.daemon = True
+        send_heartbeats_thread.daemon = True
+        election_timeout_thread.start()
+        send_heartbeats_thread.start()
         server.serve_forever()
-
-
-    
